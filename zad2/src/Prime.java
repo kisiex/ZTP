d1f86b660027e198dbc7327571c71381b1b82cc7
Kisiex <kisielewski.adam@gmail.com>
2018-10-24 23:11:11 +0200

    # added working version, need to beautify # need to valid servlet name

--- zad2/src/Prime.java
+++ zad2/src/Prime.java
@@ -8,19 +8,58 @@ import java.io.IOException;
 
 @WebServlet(name = "Prime131726")
 public class Prime extends HttpServlet {
+
+    //3k + 7 >= n && isPrime( 3k + 7 )
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int number = Integer.parseInt(request.getParameter("n"));
+        int first = (number - 7) / 3;
 
-        System.out.println(number);
+        first = 3 * first + 7;
+        while (first > number) {
+            first -= 3;
+        }
+
+        while (true) {
+            if (isPrime(first)) {
+                break;
+            }
+            first -= 3;
+        }
+
         ServletOutputStream out = response.getOutputStream();
-        out.print(number);
+        out.print(first);
     }
 
+    //3k + 7 < n && isPrime( 3k + 7 )
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int number = Integer.parseInt(request.getParameter("n"));
+        int first = (number - 7) / 3;
 
-        System.out.println(number);
+        first = 3 * first + 7;
+        while (first < number) {
+            first += 3;
+        }
+
+        while (true) {
+            if (isPrime(first)) {
+                break;
+            }
+            first += 3;
+        }
+
         ServletOutputStream out = response.getOutputStream();
-        out.print(number);
+        out.print(first);
+    }
+
+    private boolean isPrime(int number) {
+        if (number % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= number; i += 2) {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
     }
 }
+
