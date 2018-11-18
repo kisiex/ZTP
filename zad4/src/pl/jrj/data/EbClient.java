13a377ed57b59399efb0dc456c7ea289e5f44e7c
Kisiex <kisielewski.adam@gmail.com>
2018-11-18 15:51:25 +0100

    fixed calculating distance

--- zad4/src/pl/jrj/data/EbClient.java
+++ zad4/src/pl/jrj/data/EbClient.java
@@ -3,8 +3,16 @@ package pl.jrj.data;
 import javax.ejb.EJB;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EbClient {
+
+    private static final Level level = Level.FINER;
+    private static final Logger log = Logger.getLogger("EbClient");
+    private static final Handler handler = new ConsoleHandler();
 
     private Straight straight;
     private List<MassPoint> points = new ArrayList<>();
@@ -13,16 +21,22 @@ public class EbClient {
     public IDataMonitor dataMonitor;
 
     public static void main(String args[]) {
+        log.setLevel(level);
+        handler.setLevel(level);
+        log.addHandler(handler);
         System.out.println(new EbClient().calculate());
     }
 
     private double calculate() {
+        log.fine("Calculating");
         straight = new Straight(dataMonitor.next(), dataMonitor.next(), dataMonitor.next());
+        log.fine("got straight");
         while (dataMonitor.hasNext()) {
             points.add(new MassPoint(dataMonitor.next(), dataMonitor.next(), dataMonitor.next(), dataMonitor.next()));
+            log.fine("got point: #" + points.size());
         }
 
-        return points.stream().mapToDouble(point -> straight.calcDistance(point) * point.getMass()).sum();
+        return points.stream().mapToDouble(point -> Math.pow(straight.calcDistance(point), 2) * point.getMass()).sum();
     }
 
     class Point {
@@ -49,6 +63,14 @@ public class EbClient {
             this.z = z;
         }
 
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    ", z=" + z +
+                    '}';
+        }
     }
 
     class MassPoint extends Point {
@@ -79,22 +101,26 @@ public class EbClient {
             this.p2 = new Point(2 * b, -3 * a, 9 * a / (2 * c));
         }
 
-
         private double calcDistance(Point other) {
-            Point vectorPoints = new Point(p2.getX() - other.getX(),
-                    p2.getY() - other.getY(),
-                    p2.getZ() - other.getZ());
-            Point vectorMul = new Point(p2.getY() * vectorPoints.getZ(),
-                    p2.getZ() * vectorPoints.getX(),
-                    p2.getX() * vectorPoints.getY());
+            log.fine("point: " + other.toString());
+            Point vectorPoints = new Point(other.getX() - p2.getX(),
+                    other.getY() - p2.getY(),
+                    other.getZ() - p2.getZ());
 
-            return Math.sqrt(Math.pow(vectorMul.getX(), 2) + Math.pow(vectorMul.getY(), 2) + Math.pow(vectorMul.getZ(), 2))
+            log.fine("VectorPoints: " + vectorPoints.toString());
+            Point vectorMul = new Point(p2.getY() * vectorPoints.getZ() - p2.getZ() * vectorPoints.getY(),
+                    p2.getZ() * vectorPoints.getX() - p2.getX() * vectorPoints.getZ(),
+                    p2.getX() * vectorPoints.getY() - p2.getY() * vectorPoints.getX());
+            log.fine("vectorMul: " + vectorMul.toString());
+
+
+            double distance = Math.sqrt(Math.pow(vectorMul.getX(), 2) + Math.pow(vectorMul.getY(), 2) + Math.pow(vectorMul.getZ(), 2))
                     /
-                    Math.sqrt(Math.pow(vectorPoints.getX(), 2) + Math.pow(vectorPoints.getY(), 2) + Math.pow(vectorPoints.getZ(), 2));
+                    Math.sqrt(Math.pow(p2.getX(), 2) + Math.pow(p2.getY(), 2) + Math.pow(p2.getZ(), 2));
+
+            log.fine("Distance: " + distance);
+
+            return distance;
         }
-
-
     }
-
-
 }
